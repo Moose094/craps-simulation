4f9fc876c0f6cf190c9c224a41c19b54b50c47f3
diaby <diabymoussa866@gmail.com>
2020-03-01 20:30:25 -0600

    Final commit for h2

---
+++ CrapsSimulation/test/CrapsSimulationTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CrapsSimulationTest
+{
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
